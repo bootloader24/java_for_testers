eeb14acf487baf97d3a47a5c4650141b7a2fd6f1
Artem.Butin <Artem.Butin@infotecs.ru>
2023-10-11 18:11:56 +0500

    Collections intro
    lec. 4.1

---
+++ sandbox/src/test/java/ru/training/collections/CollectionTests.java
@@ -0,0 +1,29 @@
+package ru.training.collections;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import java.util.ArrayList;
+import java.util.List;
+
+public class CollectionTests {
+    @Test
+    void arrayTests() {
+        var array = new String[]{"a", "b", "c"};
+        Assertions.assertEquals(3, array.length);
+        array[0] = "a";
+        Assertions.assertEquals("a", array[0]);
+
+        array[0] = "d";
+        Assertions.assertEquals("d", array[0]);
+    }
+
+    @Test
+    void listTests() {
+        var list = new ArrayList<>(List.of("a", "b", "c"));
+        Assertions.assertEquals(3, list.size());
+        Assertions.assertEquals("a", list.get(0));
+
+        list.set(0, "d");
+        Assertions.assertEquals("d", list.get(0));
+    }
+}
