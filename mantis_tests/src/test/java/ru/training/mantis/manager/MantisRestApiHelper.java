79f34c33f370fa026a422fee723f796f095f4c15
Artem.Butin <Artem.Butin@infotecs.ru>
2023-11-15 23:19:07 +0500

    Creating issue test added (REST API)
    lec. 9.5

--- mantis_tests/src/test/java/ru/training/mantis/manager/MantisRestApiHelper.java
+++ mantis_tests/src/test/java/ru/training/mantis/manager/MantisRestApiHelper.java
@@ -3,11 +3,15 @@ package ru.training.mantis.manager;
 import io.swagger.client.ApiClient;
 import io.swagger.client.ApiException;
 import io.swagger.client.Configuration;
+import io.swagger.client.api.IssuesApi;
 import io.swagger.client.api.UserApi;
 import io.swagger.client.auth.ApiKeyAuth;
+import io.swagger.client.model.Identifier;
+import io.swagger.client.model.Issue;
 import io.swagger.client.model.User;
 import io.swagger.client.model.UserAddResponse;
 import ru.training.mantis.model.UserData;
+import ru.training.mantis.model.IssueData;
 
 public class MantisRestApiHelper extends HelperBase {
 
@@ -36,6 +40,25 @@ public class MantisRestApiHelper extends HelperBase {
         UserApi apiInstance = new UserApi();
         try {
             apiInstance.userDelete(id);
+        } catch (ApiException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void createIssue(IssueData issueData) {
+        Issue issue = new Issue();
+        issue.setSummary(issueData.summary());
+        issue.setDescription(issueData.description());
+        var projectId = new Identifier();
+        projectId.setId(issueData.project());
+        issue.setProject(projectId);
+        var categoryId = new Identifier();
+        categoryId.setId(issueData.category());
+        issue.setCategory(categoryId);
+
+        IssuesApi apiInstance = new IssuesApi();
+        try {
+            apiInstance.issueAdd(issue);
         } catch (ApiException e) {
             throw new RuntimeException(e);
         }
---
+++ mantis_tests/src/test/java/ru/training/mantis/tests/IssueCreationTests.java
@@ -0,0 +1,17 @@
+package ru.training.mantis.tests;
+
+import org.junit.jupiter.api.Test;
+import ru.training.mantis.common.CommonFunctions;
+import ru.training.mantis.model.IssueData;
+
+public class IssueCreationTests extends TestBase{
+
+    @Test
+    void canCreateIssue() {
+        app.mantisRestApi().createIssue(new IssueData()
+                .withSummary(CommonFunctions.randomString(10))
+                .withDescription(CommonFunctions.randomString(20))
+                .withProject(1L)
+                .withCategory(1L));
+    }
+}
